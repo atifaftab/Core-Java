44c741aad42ad49f5e6085dc7abd21f1284d8058
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-05-28 17:48:34 +0800

    Pattern 7 type completed

---
+++ src/com/patern/Pattern7.java
@@ -0,0 +1,14 @@
+package com.patern;
+
+import java.util.Scanner;
+
+public class Pattern7 {
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number for stars ");
+        int n = in.nextInt();
+
+
+    }
+}
