f55262b201b6fa5d6ecbcd3eab2f37852f128f8a
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-05-31 07:12:07 +0800

    Reverse String and test

--- src/com/string/ReverseString1.java
+++ src/com/string/ReverseString1.java
@@ -3,9 +3,17 @@ package com.string;
 import java.util.Scanner;
 
 public class ReverseString1 {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
 
         System.out.println("Enter th");
+
+        String source = "Atif";
+        String reverse = "";
+
+        for (int i = source.length() - 1; i >= 0; i--) {
+            reverse = reverse + source.charAt(i);
+        }
+        System.out.println(reverse);
     }
 }
