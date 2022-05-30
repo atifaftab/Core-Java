4a83b08bfa553358406bfeefcd3efcd616c35c57
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-05-30 12:29:07 +0800

    exceptions demo

--- src/com/exceptions/ExceptionExample.java
+++ src/com/exceptions/ExceptionExample.java
@@ -3,5 +3,12 @@ package com.exceptions;
 public class ExceptionExample {
     public static void main(String[] args) {
 
+        try {
+            String str = null;
+            System.out.println(str.length());
+        }
+        catch (NullPointerException e){
+            System.out.println(e);
+        }
     }
 }
