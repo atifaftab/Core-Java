5b1704f9b52ebe6a92f7908ba5b35d4134b4df02
Mohammad Atif Aftab <iamatif96@gmail.com>
2022-06-02 09:23:14 +0800

    functional Interface Implemented

--- src/com/interfaces/impl/FunctionalInterfaceImpl.java
+++ src/com/interfaces/impl/FunctionalInterfaceImpl.java
@@ -1,4 +1,11 @@
 package com.interfaces.impl;
 
+import com.interfaces.FunctionalInterfaceDemo;
+
 public class FunctionalInterfaceImpl {
+
+    public static void main(String[] args) {
+        FunctionalInterfaceDemo obj = () -> System.out.println(" Hell yeah");
+        obj.print();
+    }
 }
